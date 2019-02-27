ac5ff1d9868d4b04da158ef6ff37f8b276fc28a9
taahmed <46457860+taahmed@users.noreply.github.com>
2019-02-27 12:07:33 -0800

    Assignment - 6

--- pythonclubproj/club/views.py
+++ pythonclubproj/club/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
+
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
 # importing all theobjects under the productstype
-def clubmeetings(request):
+def clubresources(request):
+    resources_list=Resource.objects.all()  
+    return render (request, 'club/resources.html',{'resources_list': resources_list})
+
+def getmeeting(request):
     meetings_list=Meeting.objects.all()  
     return render (request, 'club/meetings.html',{'meetings_list': meetings_list})
+
+def meetingdetail(request, id):
+    detail=get_object_or_404(Meeting, pk=id)  
+    context={'detail': detail}
+    return render (request, 'club/details.html',context= context)
 
 def clubminutes(request):
     minutes_list=MeetingMinutes.objects.all()  
     return render (request, 'club/minutes.html',{'minutes_list': minutes_list})
-
-def clubresources(request):
-    resources_list=Resource.objects.all()  
-    return render (request, 'club/resources.html',{'resources_list': resources_list})
 
 def clubevents(request):
     events_list=Event.objects.all()  
