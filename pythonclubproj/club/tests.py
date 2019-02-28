51564fba6ce30e2e34165af688e2c1e091e5599f
taahmed <46457860+taahmed@users.noreply.github.com>
2019-02-28 12:41:22 -0800

    Assignment - 7

--- pythonclubproj/club/tests.py
+++ pythonclubproj/club/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Resource, Meeting 
+from django.urls import reverse
 
 # Create your tests here.
+class ResourceTest(TestCase):
+    def test_stringOutput(self):
+        resource=Resource(resourcename='lenovo')
+        self.assertEqual(str(resource), resource.resourcename)
+    
+    def test_tablename(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+
+class MeetingTest(TestCase):
+    def test_stringOutput(self):
+        meeting=Meeting(meetingtitle='Salary Increment')
+        self.assertEqual(str(meeting), meeting.meetingtitle)
+
+    def test_tablename(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+
+
